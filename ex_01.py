a2cbdb49836e904a14e757a44793bb4e271ab874
eqpeter <87522471+eqpeter@users.noreply.github.com>
2025-03-08 18:09:09 +0800

    Update ex_01.py

--- ex_01.py
+++ ex_01.py
@@ -24,44 +24,91 @@
 #    2)印出：2/3 的倒數是 3/2，等於 1.5
 #########################################################################
 
-## Methods to add and multiply two Fraction objects
 class Fraction(object):
-    def __init__(self, top, bottom):
-        '''
-        Constructor of Fraction class'''
+  def __init__(self, top, bottom):
+      '''
+      分數類別的構造函數，初始化分子和分母
+      '''
+      self.num = top
+      self.den = bottom
 
+  def __add__(self, other_fraction):
+      '''
+      定義特殊方法實現加法運算，返回一個新的分數對象
+      '''
+      new_num = self.num * other_fraction.den + self.den * other_fraction.num
+      new_den = self.den * other_fraction.den
+      return Fraction(new_num, new_den)
 
-    def __add__(self, other_fraction):
-        '''
-        Defines special method to implement + operator;
-        returns a new Fraction object'''
+  def __sub__(self, other_fraction):
+      '''
+      定義特殊方法實現減法運算，返回一個新的分數對象
+      '''
+      new_num = self.num * other_fraction.den - self.den * other_fraction.num
+      new_den = self.den * other_fraction.den
+      return Fraction(new_num, new_den)
 
+  def __mul__(self, other_fraction):
+      '''
+      定義特殊方法實現乘法運算，返回一個新的分數對象
+      '''
+      new_num = self.num * other_fraction.num
+      new_den = self.den * other_fraction.den
+      return Fraction(new_num, new_den)
 
-    def __sub__(self, other_fraction):
-        '''
-        Defines special method to implement - operator;
-        returns a new Fraction object'''
+  def __truediv__(self, other_fraction):
+      '''
+      定義特殊方法實現除法運算，返回一個新的分數對象
+      '''
+      new_num = self.num * other_fraction.den
+      new_den = self.den * other_fraction.num
+      return Fraction(new_num, new_den)
 
+  def __str__(self):
+      '''
+      方法來打印分數對象
+      '''
+      return f"{self.num}/{self.den}"
 
-    def __mul__(self, other_fraction):
-        '''
-        Special method to multiply 2 Fraction objects;
-        returns a new Fraction object'''
+  def invert(self):
+      '''
+      方法來倒轉分數對象
+      '''
+      return Fraction(self.den, self.num)
 
+  def value(self):
+      '''
+      方法來計算分數的值
+      '''
+      return self.num / self.den
 
-    def __str__(self):
-        '''Method to print a Fraction object'''
-
-
-    def invert(self):
-        '''Method to invert the Fraction object'''
-
-
-    def value(self):
-        '''Method to calculate into a value'''
-
+  def value_as_str(self):
+      '''
+      方法來計算分數的值並以字串形式返回（小數點後面沒有0）
+      '''
+      value = self.num / self.den
+      if value.is_integer():
+          return str(int(value))
+      else:
+          return f"{value:.3f}".rstrip('0').rstrip('.')
 
 # +++加入你的程式碼+++
 # 呼叫你完成的函數
+
+# 假設我們有以下股票的市場價格和每月配息
+market_price = Fraction(1100, 1)  # 市場價格 1100 元/股
+monthly_dividend_per_share = Fraction(15, 10)  # 每月每股配息 1.5 元（用15/10表示）
+
+# 計算每年的總配息
+annual_dividend_per_share = monthly_dividend_per_share * Fraction(12, 1)
+print(f"台積電股價1100元\n計畫改月配息每股: {monthly_dividend_per_share.value_as_str()} 元")
+print(f"每年可獲股息: 等於 {annual_dividend_per_share.value_as_str()} 元")
+
+# 計算殖利率
+dividend_yield = annual_dividend_per_share / market_price
+print(f"殖利率: 等於 {dividend_yield.value_as_str()}%")
+# 評論
+print()
+print("評論: \n台積電股配息太低了?\n還是股價漲太高?")
 
 
